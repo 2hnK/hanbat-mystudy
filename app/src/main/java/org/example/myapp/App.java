0e69d72ee4f67e61db24d34d024e4759db15d1dc
2hnK <kimgh921@gmail.com>
2025-07-30 14:59:54 +0900

    day-3

--- app/src/main/java/org/example/myapp/App.java
+++ app/src/main/java/org/example/myapp/App.java
@@ -1,30 +1,23 @@
 package org.example.myapp;
 
-import org.example.utils.Utils;
-
-import com.google.common.base.Joiner;
+import java.util.Scanner;
 
 /**
- * App 클래스는 "Hello" 메시지를 콘솔에 출력하는 간단한 자바 애플리케이션입니다.
- * <p>
- * 이 클래스는 프로그램의 진입점인 {@code main} 메서드를 포함하고 있습니다.
+ * This is a simple Java application that prompts the user for their name
+ * and greets them.
  *
- * @author KimJiHun
- * @version 1.0
- * @since 2025-07-29
+ * @author 2hnK
  */
 public class App {
-
-    /**
-     * 프로그램의 실행이 시작되는 메인 메서드입니다.
-     * <p>
-     * "Hello"를 표준 출력으로 인쇄합니다.
-     *
-     * @param args 명령줄 인수로, 이 애플리케이션에서는 사용되지 않습니다.
+    /*
+     * This is a simple Java application that prompts the user for their name
      */
+
     public static void main(String[] args) { // entry point
-        Utils.printMessage("Hello!");
-        String message = Joiner.on(", ").join("Hello", "world!");
-        System.out.println(message);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + "!");
+        sc.close();
     }
 }
